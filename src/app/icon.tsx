10b813f50eafba7e49ea522e0ec1f01694c72824
Leo Gavin <leogavin123@outlook.com>
2024-11-27 18:15:49 +0530

    Update icon.tsx

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,8 +1,5 @@
 import { ImageResponse } from 'next/og'
  
-// Route segment config
-export const runtime = 'edge'
- 
 // Image metadata
 export const size = {
   width: 32,
@@ -14,26 +11,22 @@ export const contentType = 'image/png'
 export default function Icon() {
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
           fontSize: 20,
           fontWeight: 700,
-          background: 'linear-gradient(45deg, #47FFB2, #47D1FF)',
+          background: '#000',
           width: '100%',
           height: '100%',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          borderRadius: '50%',
-          color: '#1A1A1A',
-          boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+          color: '#fff',
         }}
       >
         LF
       </div>
     ),
-    // ImageResponse options
     {
       ...size,
     }
