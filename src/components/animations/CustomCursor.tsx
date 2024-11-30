fefe000e9c414ef74f0e2caf90d3c256c09409b4
Leo Gavin <leogavin123@outlook.com>
2024-11-30 12:40:53 +0530

    Update CustomCursor.tsx

--- src/components/animations/CustomCursor.tsx
+++ src/components/animations/CustomCursor.tsx
@@ -1,4 +1,4 @@
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
 import { useEffect } from "react";
 
 interface CustomCursorProps {
@@ -8,9 +8,6 @@ interface CustomCursorProps {
 export default function CustomCursor({ isHovered }: CustomCursorProps) {
   const cursorX = useMotionValue(-100);
   const cursorY = useMotionValue(-100);
-  const springConfig = { damping: 25, stiffness: 700 };
-  const cursorXSpring = useSpring(cursorX, springConfig);
-  const cursorYSpring = useSpring(cursorY, springConfig);
 
   useEffect(() => {
     const moveCursor = (e: MouseEvent) => {
@@ -29,24 +26,24 @@ export default function CustomCursor({ isHovered }: CustomCursorProps) {
       <motion.div
         className="cursor-outer"
         style={{
-          left: cursorXSpring,
-          top: cursorYSpring,
+          left: cursorX,
+          top: cursorY,
           scale: isHovered ? 1.5 : 1,
         }}
       />
       <motion.div
         className="cursor-inner"
         style={{
-          left: cursorXSpring,
-          top: cursorYSpring,
+          left: cursorX,
+          top: cursorY,
           scale: isHovered ? 0.5 : 1,
         }}
       />
       <motion.div
         className="cursor-core"
         style={{
-          left: cursorXSpring,
-          top: cursorYSpring,
+          left: cursorX,
+          top: cursorY,
           scale: isHovered ? 1.2 : 1,
         }}
       />
